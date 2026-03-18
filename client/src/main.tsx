0c78aca73b2d791b97d6be21357d75f3b95f96d8
Aditya raj <adityaraj7320@gmail.com>
2026-03-19 02:01:08 +0530

    frontend

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 import ProtectedRoute from "./components/ProtectedRoute";
