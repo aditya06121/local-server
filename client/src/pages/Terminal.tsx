3b37e573aaccbf65e89528d242c2a848e36949bc
Aditya raj <adityaraj7320@gmail.com>
2026-04-12 22:22:56 +0530

    wetty

--- client/src/pages/Terminal.tsx
+++ client/src/pages/Terminal.tsx
@@ -1,17 +1,9 @@
+import { useEffect } from "react";
+
 export default function Terminal() {
-  return (
-    <iframe
-      src="/wetty"
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100vw",
-        height: "100vh",
-        border: "none",
-        zIndex: 1300,
-      }}
-      title="Terminal"
-    />
-  );
+  useEffect(() => {
+    window.location.replace("/wetty");
+  }, []);
+
+  return null;
 }
